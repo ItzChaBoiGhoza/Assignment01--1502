ac19c946b042586db16e90c990e23db29c429c64
Ghoza <cguy925@gmail.com>
2022-01-26 01:24:06 -0700

    Added main menu method inside AppMenu and Player.java

--- Assignment 1 - Skeleton/src/mru/game/model/Player.java
+++ Assignment 1 - Skeleton/src/mru/game/model/Player.java
@@ -6,4 +6,43 @@ public class Player {
 	 * This class represent each player record in the Database
 	 * It is basically a model class for each record in the txt file
 	 */
+	
+	String name;
+	String id;
+	int numOfWins;
+	
+	
+	public Player(String name, String id, int numOfWins) {
+		this.name = name;
+		this.id = id;
+		this.numOfWins = numOfWins;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public void setId(String id) {
+		this.id = id;
+	}
+	
+	public void setWins(int numOfWins) {
+		this.numOfWins = numOfWins;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public String getId() {
+		return id;
+	}
+	
+	public int getWins() {
+		return numOfWins;
+	}
+	
+	public String toString() {
+		return "Name: " + name + " ID: " + id + " Wins: " + numOfWins;
+	}
 }
--- Assignment 1 - Skeleton/src/mru/game/view/AppMenu.java
+++ Assignment 1 - Skeleton/src/mru/game/view/AppMenu.java
@@ -1,10 +1,26 @@
 package mru.game.view;
 
+import java.util.Scanner;
 
 public class AppMenu {
+	
+	Scanner input;
+	
+	public AppMenu() {
+		input = new Scanner(System.in);
+	}
 
 	/**
 	 * This class will be used to show the menus and sub menus to the user
 	 * It also prompts the user for the inputs and validates them 
 	 */
+	public void showMainMenu() {
+		System.out.println("Choose one of the option:\n");
+		System.out.println("\t1. Play Game");
+		System.out.println("\t2. Search");
+		System.out.println("\t3. Save and Exit\n");
+		System.out.print("Enter your option: ");
+	}
+	
+	
 }
