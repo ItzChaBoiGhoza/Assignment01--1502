fa9d1333b4f72bcedc6fa28094a2cd3326ef2b40
Ghoza <cguy925@gmail.com>
2022-02-11 11:47:30 -0700

    Add comments, JavaDoc and Runnable Jar file

--- Assignment 1 - Skeleton/src/mru/game/controller/PuntoBancoGame.java
+++ Assignment 1 - Skeleton/src/mru/game/controller/PuntoBancoGame.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import mru.game.model.Player;
 import mru.game.view.GameOptions;
 
+//Note: not completed yet
 public class PuntoBancoGame extends CardDeck{
 	
 	/**
@@ -12,17 +13,23 @@ public class PuntoBancoGame extends CardDeck{
 	 * You should use CardDeck class here
 	 * See the instructions for the game rules
 	 */
-	char winChoice;
-	private int playerDeck;
-	private int bankerDeck;
-	int cashTotal;
-	int wins = 0;
 	
-	CardDeck cd;
-	GameOptions go;
-	GameManager gm;
-	ArrayList<Player> players;
+	char winChoice; 
+	private int playerDeck; 
+	private int bankerDeck; 
+	int cashTotal; 
+	int wins = 0; 
 	
+	CardDeck cd; 
+	GameOptions go; 
+	GameManager gm; 
+	ArrayList<Player> players; 
+	
+	/**
+	 * Constructor
+	 * @param money
+	 * @throws Exception
+	 */
 	public PuntoBancoGame(int money) throws Exception {
 		cd = new CardDeck();
 		go = new GameOptions();
@@ -32,6 +39,9 @@ public class PuntoBancoGame extends CardDeck{
 		playGame();
 	}
 	
+	/**
+	 * playGame method
+	 */
 	public void playGame() {
 		startGame();
 		go = new GameOptions();
@@ -39,22 +49,28 @@ public class PuntoBancoGame extends CardDeck{
 		go.gameMenu();
 	}
 	
+	/**
+	 * Method to getWins for the user
+	 * @return
+	 */
 	public int getWins() {
 		return wins;
 	}
 	
+	/**
+	 * Initialization of the game
+	 * Get card for the player and banker
+	 */
 	public void startGame() {
 		Card playerHand; 
 		Card bankerHand;
 		
+		//Deal two cards to the player
 		playerHand = cd.getDeck().remove(0);
 		playerHand = cd.getDeck().remove(0);
 		
+		//Deal two cars to the banker
 		bankerHand = cd.getDeck().remove(0);
 		bankerHand = cd.getDeck().remove(0);
-	}
-
-	public void playerScore(int rank) {
-		
 	}
 }
--- Assignment 1 - Skeleton/src/mru/game/view/GameOptions.java
+++ Assignment 1 - Skeleton/src/mru/game/view/GameOptions.java
@@ -6,9 +6,12 @@ import mru.game.model.Player;
 
 public class GameOptions {
 	
-	private Scanner input;
-	Player player;
+	private Scanner input; 
+	Player player; 
 	
+	/**
+	 * Constructor
+	 */
 	public GameOptions() {
 		input = new Scanner(System.in);
 	}
@@ -19,10 +22,14 @@ public class GameOptions {
 	 */
 	public int betAmount() {
 		
-		int bet = 0;
-		boolean validBet = false;
+		int bet = 0; //Initialize the bet
+		boolean validBet = false; //Initialize the validBet
+		
+		//A loop that will check for user input on how much they want to bet
 		while(validBet == false) {
 			System.out.print("How much do you want to bet this round? ");
+			
+			//Checks if the user has enough amount of cash to bet
 			if(!input.hasNextInt()) {
 				bet = input.nextInt();
 				input.nextLine();
@@ -42,7 +49,7 @@ public class GameOptions {
 	}
 	
 	/**
-	 * Method to display gamemenu
+	 * Method to display game menu
 	 * @return option
 	 */
 	public char gameMenu() {
